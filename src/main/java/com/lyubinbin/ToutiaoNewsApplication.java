4b25dba6e690e3a62bdf363d39b244b5a1487705
LyricistPKU <lvbinbinpku@gmail.com>
2016-07-27 22:55:00 +0800

    add conversation service

--- src/main/java/com/lyubinbin/ToutiaoNewsApplication.java
+++ src/main/java/com/lyubinbin/ToutiaoNewsApplication.java
@@ -2,13 +2,15 @@ package com.lyubinbin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToutiaoNewsApplication {
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(ToutiaoNewsApplication.class);
-//	}
+public class ToutiaoNewsApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ToutiaoNewsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToutiaoNewsApplication.class, args);
--- src/main/java/com/lyubinbin/controller/HomeController.java
+++ src/main/java/com/lyubinbin/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.lyubinbin.controller;
 
-import com.lyubinbin.model.EntityType;
-import com.lyubinbin.model.Hostholder;
-import com.lyubinbin.model.News;
-import com.lyubinbin.model.ViewObject;
+import com.lyubinbin.model.*;
 import com.lyubinbin.service.LikeService;
+import com.lyubinbin.service.MessageService;
 import com.lyubinbin.service.NewsService;
 import com.lyubinbin.service.UserService;
 import com.lyubinbin.util.MailSender;
+import com.lyubinbin.util.ToutiaoUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,8 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,6 +28,8 @@ import java.util.List;
  */
 @Controller
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     NewsService newsService;
     @Autowired
@@ -35,6 +40,8 @@ public class HomeController {
     Hostholder hostholder;
     @Autowired
     MailSender mailSender;
+    @Autowired
+    MessageService messageService;
 
     // for return news list viewobject
     private List<ViewObject> getNews(int userId, int offset, int limit){
@@ -83,6 +90,7 @@ public class HomeController {
             pop = 0;
         }
         model.addAttribute("pop", pop);
+        //get latest 20 news
         model.addAttribute("vos", getLastesNews(0, 20));
         model.addAttribute("user", hostholder.getUser());
         return "home";
@@ -90,8 +98,34 @@ public class HomeController {
 
     @RequestMapping(path = {"/user/{userId}"}, method = {RequestMethod.POST, RequestMethod.GET})
     public String userIndex(Model model, @PathVariable("userId") int userId){
+        //get latest 10 news of a user
         model.addAttribute("vos", getNews(userId, 0, 10));
         model.addAttribute("user", hostholder.getUser());
-        return "home";
+        model.addAttribute("target", userService.getUser(userId));
+        return "user";
+    }
+
+    // add message and redirect to the same page
+    @RequestMapping(path = {"/addMessage"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String addMessage(@RequestParam("fromId") int fromId,
+                             @RequestParam("toId") int toId,
+                             @RequestParam("content") String content){
+        System.out.println("Doing add mssage");
+        try{
+            content = HtmlUtils.htmlEscape(content);
+            if(fromId != toId && content != null){
+                Message message = new Message();
+                message.setFromId(fromId);
+                message.setToId(toId);
+                message.setContent(content);
+                message.setCreatedDate(new Date());
+                message.setHasRead(0);
+                message.setConversationId(ToutiaoUtil.getConversationId(fromId, toId));
+                messageService.addMessage(message);
+            }
+        }catch (Exception e){
+            logger.error("Add Message Failed: " + e.getMessage());
+        }
+        return "redirect:/user/" + String.valueOf(toId);
     }
 }
